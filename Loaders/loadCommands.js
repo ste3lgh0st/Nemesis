4562dcfa07b4395570b463ffce57f14657e410b2
ste3lgh0st <eddievanhalen.fratworld@gmail.com>
2026-07-24 22:25:25 +0200

    Fix casensensitivity in loadCommands

--- Loaders/loadCommands.js
+++ Loaders/loadCommands.js
@@ -4,7 +4,7 @@ module.exports = async bot => {
 
     fs.readdirSync("./Commandes").filter(f => f.endsWith(".js")).forEach(async file => {
 
-        let command = require(`../commandes/${file}`)
+        let command = require(`../Commandes/${file}`);
         if(!command.name || typeof command.name !=="string") throw new TypeError(`La commande ${file.slice(0, file.lengh - 3)}n'a pas de nom !`)
         bot.commands.set(command.name, command)
     console.log(`Commande ${file} chargée avec succès`)
